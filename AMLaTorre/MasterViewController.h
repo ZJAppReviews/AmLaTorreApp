166d04ed1799c371add98f31efb7d48ccddddf7c
Marco <mba121@gmail.com>
2014-11-20 21:10:42 +0100

    Initial Commit

---
+++ AMLaTorre/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  AMLaTorre
+//
+//  Created by Marco on 20/11/14.
+//  Copyright (c) 2014 tmac12. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
